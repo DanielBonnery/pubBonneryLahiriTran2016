b461fb4f865b0807c1619eaca13463d9ae9b7ed9
DanielBonnery <daniel.bonnery@gmail.com>
2016-07-11 09:38:16 +0200

    a

--- demo/descriptive.R
+++ demo/descriptive.R
@@ -15,7 +15,7 @@ ggplot(data=xy,aes(x=ftemp07,y=ftemp12))+geom_point()+geom_abline(color="red",in
 ggplot(data=xy,aes(x=ftemp07,y=ftemp12))+geom_point()+geom_abline(color="red",intercept=0,slope=1)
 ggplot(data=xy,aes(x=lftemp07,y=lftemp12))+geom_point()+geom_abline(color="red",intercept=0,slope=1)
 
-plot1<-ggplot(data = xy, aes(x = ftemp07, y = ftemp12)) +
+plot1<-ggplot(data = xys, aes(x = ftemp07, y = ftemp12)) +
   geom_point(size=1) +   geom_smooth(method="lm",se=FALSE) +geom_abline(slope=1,intercept=0,color="red")+
 facet_wrap(~type_of_gov)
 save(plot1,file="figure/plot1.rda")
@@ -28,21 +28,22 @@ save(plot2,file="figure/plot2.rda")
 plot2bis<-ggplot(data = xy, aes(x = lftemp07, y = lftemp12)) + 
   stat_density2d(geom="tile", aes(fill=..density..^0.1), contour=FALSE) +
   scale_fill_gradientn(colours = colorRampPalette(c("white", blues9))(256)) +
-  geom_smooth(method="lm",se=FALSE) +geom_abline(slope=1,intercept=0,color="red")+
+  geom_smooth(method="lm",se=FALSE) +geom_abline(slope=1,intercept=0,color="red")+ 
   facet_wrap(~type_of_gov)
 save(plot2bis,file="figure/plot2bis.rda")
 
 
 
 plot2bisbis<-ggplot(data = xy, aes(x = lftemp07, y = lftemp12)) + 
+  geom_smooth(method="lm",se=FALSE) +geom_abline(slope=1,intercept=0,color="red")+
   stat_density2d(aes(alpha=..level..^.1, fill=..level..^.1), 
-                 size=2, 
+                 size=6, 
                  bins=4, 
                  geom="polygon") + 
   scale_fill_gradient(low = "yellow", high = "red") +
   #scale_alpha(range = c(0.00, 1), guide = FALSE) +
   geom_density2d(colour="grey", bins=4) +
-  guides(alpha=FALSE) +
+  guides(alpha=FALSE) + #  
 facet_wrap(~type_of_gov)
   
 
