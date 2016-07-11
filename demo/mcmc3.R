fe117095cb9093733462580b480aa29e92c91c74
DanielBonnery <daniel.bonnery@gmail.com>
2016-07-11 10:58:01 +0200

    a

--- demo/mcmc3.R
+++ demo/mcmc3.R
@@ -8,26 +8,36 @@ data(aspep2007,aspep2011,aspep2012,aspep2007_gov)
 xy<-xyf(aspep2007, aspep2012, aspep2007_gov)
 xys<-xysf(aspep2011,xy)
 xr<-xrf(xy,xys)
-xys_aggrd<-sqldf("select count(*) as n, state,itemcode, type_of_gov, sum(lftemp12) as lftemp12, sum(lftemp07) as lftemp07 
-                 from xys group by type_of_gov, itemcode, state")
 
-#3. Computations
 usefullvariables<-c("state","itemcode","type_of_gov","lftemp12","lftemp07")
 dime<-sapply(xys[c("state","itemcode","type_of_gov")],nlevels)
 dimnamesd<-sapply(xys[c("state","itemcode","type_of_gov")],levels)
 
 #3.2. model we discussed
 fit2 <-jags(
-  data=c(list(N=nrow(xys),dime=dime,xy=xy),xy_aggrd[usefullvariables]),
-  inits=list(list("a1"=array(0,dime),"beta1"=array(0,dime),"tau"=array(1,dime))),
+  data=c(list(N=nrow(xys),dime=dime),xys[usefullvariables]),
+  inits=list(list("a1"=rep(0,dime[1]),
+                  "a2"=rep(0,dime[2]),
+                  "a3"=rep(0,dime[3]),
+                  "b1"=array(0,dime[-1]),
+                  "b2"=array(0,dime[-2]),
+                  "b3"=array(0,dime[-3]),
+                  "c1"=rep(0,dime[1]),
+                  "c2"=rep(0,dime[2]),
+                  "c3"=rep(0,dime[3]),
+                  "d1"=array(0,dime[-1]),
+                  "d2"=array(0,dime[-2]),
+                  "d3"=array(0,dime[-3]),
+                  "tau_1"=matrix(1,2,3),
+                  "sigma_1"=1)),
   n.chains =1,
-  parameters.to.save=c("sigma","beta0", "beta1"),
+  parameters.to.save=c(outer(letters[1:4],1:3,paste0),,"beta0","beta1","tau","sigma"),
   n.iter =10 ,
   n.burnin =3 ,
   model.file= textConnection (
     "model {
     for (i in 1:N) {
-    lftemp[i]~dnorm(beta0[state[i],itemcode[i],type_of_gov[i]]+beta1[state[i],itemcode[i],type_of_gov[i]]*lftemp07[i],tau)}
+    lftemp12[i]~dnorm(beta0[state[i],itemcode[i],type_of_gov[i]]+beta1[state[i],itemcode[i],type_of_gov[i]]*lftemp07[i],sigma_1)}
     for (i1 in 1:dime[1]) {
     for (i2 in 1:dime[2]) {
     for (i3 in 1:dime[3]) {
@@ -37,19 +47,19 @@ fit2 <-jags(
     for (i2 in 1:dime[2]) {a2[i2]~dnorm(0,1.0E-4);c2[i2]~dnorm(0,1.0E-4)}
     for (i3 in 1:dime[3]) {a3[i3]~dnorm(0,1.0E-4);c3[i3]~dnorm(0,1.0E-4)}
     for (i1 in 1:dime[1]) {
-    for (i2 in 1:dime[2]) {}}
-for (i1 in 1:dime[1]) {
-    for (i3 in 1:dime[3]) {}}
-for (i2 in 1:dime[2]) {
-    for (i3 in 1:dime[3]) {}}
-
-
-    tau~ dgamma (1.0E-4 ,1.0E-4);
-    sigma <- 1/tau
+    for (i2 in 1:dime[2]) {b3[i1,i2]~dnorm(0,tau_1[1,1]);d3[i1,i2]~dnorm(0,tau_1[2,1])}}
+    for (i1 in 1:dime[1]) {
+    for (i3 in 1:dime[3]) {b2[i1,i3]~dnorm(0,tau_1[1,2]);d2[i1,i3]~dnorm(0,tau_1[2,2])}}
+    for (i2 in 1:dime[2]) {
+    for (i3 in 1:dime[3]) {b1[i2,i3]~dnorm(0,tau_1[1,3]);d1[i2,i3]~dnorm(0,tau_1[2,3])}}
+    for (i in 1:2) {for (j in 1:3){tau_1[i,j]~dgamma (1.0E-4 ,1.0E-4)}} 
+    sigma_1~ dgamma (1.0E-4 ,1.0E-4);
+    sigma <- 1/sigma_1
+    tau<-1/tau_1
     }"))
 
 d=xr[xr$state=="Alabama"&xr$itemcode=="Water Supply " &xr$type_of_gov=="Municipal government",]
-fit=fit1
+fit=fit2
 sims.list<-fit$BUGSoutput$sims.list
 predictions<-function(sims.list){
   n<-dim(sims.list$beta0)[1]
